6e5b801ec8db76ccd0b3b440edfb4d39c8ada8f8
Akangbe Olayemi <akangbeolayemi@gmail.com>
2022-11-09 01:04:12 +0100

    Booking Application created in golang

--- main.go
+++ main.go
@@ -2,23 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
-	var conferenceName = "Go conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+var conferenceName = "Go conference"
 
+const conferenceTickets = 50
+
+var remainingTickets = 50
+var userName string
+var userTicket int
+var userChoice int
+
+func welcome() {
 	fmt.Printf("Welcome to %s booking application", conferenceName)
-	fmt.Printf("\nWe have total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
+	fmt.Println("\n-----------------------------------")
+	fmt.Printf("\nWe have a total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
 	fmt.Println("\nGet your tickets here to attend")
-
-	var userName string
-	var userTicket int
-
+	fmt.Println("-----------------------------------")
+}
+func getUsername() {
 	fmt.Print("Please enter your name to book a ticket: ")
-	fmt.Scan(&userName)
-
+	_, err := fmt.Scan(&userName)
+	if err != nil {
+		return
+	}
+}
+func getUserChoice() {
 	fmt.Println("Press 1 to get a ticket or Press 2 to exit the app")
-	var userChoice int
 	fmt.Scan(&userChoice)
 
 	switch userChoice {
@@ -36,6 +44,15 @@ func main() {
 	} else {
 		remainingTickets = conferenceTickets
 	}
+}
+func main() {
+
+	welcome()
+	getUsername()
+	getUserChoice()
+
 	fmt.Printf("User %s has booked %v ticket(s)", userName, userTicket)
 	fmt.Printf("\nWe have %v tickets available for grabs...", remainingTickets)
+	fmt.Println("\n-----------------------------------")
+	fmt.Println("\nWant to make another booking?\t 1. Yes\t 2. No.")
 }
